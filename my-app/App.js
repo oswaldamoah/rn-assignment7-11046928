a8dba01d8866c6daf1b753fe0dae19c416dad0c1
oswaldamoah <oswaldamoah00@gmail.com>
2024-07-10 04:17:18 +0000

    updated App component based on ProductDetails navigation

--- my-app/App.js
+++ my-app/App.js
@@ -3,20 +3,30 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './HomeScreen';
 import CartScreen from './CartScreen';
+import ProductDetails from './ProductDetails';
 import Menu from './Menu';
 
 const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+
+const HomeStack = () => (
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="HomeScreen" component={HomeScreen} />
+    <Stack.Screen name="ProductDetails" component={ProductDetails} />
+  </Stack.Navigator>
+);
 
 const App = () => {
   return (
     <NavigationContainer>
       <Drawer.Navigator
         drawerContent={(props) => <Menu {...props} />}
-        screenOptions={{ headerShown: false }} // Ensure no default headers are shown
+        screenOptions={{ headerShown: false }}
       >
-        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Home" component={HomeStack} />
         <Drawer.Screen name="CartScreen" component={CartScreen} />
         {/* Add other screens here */}
       </Drawer.Navigator>
